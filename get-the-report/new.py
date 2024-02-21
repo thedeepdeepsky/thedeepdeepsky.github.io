ea64bde100974c3bf2f0dbcfda9ca4a55a7580cc
Liu Hengyuan <110045428+thedeepdeepsky@users.noreply.github.com>
2024-02-21 22:16:47 +0800

    Update new.py

--- get-the-report/new.py
+++ get-the-report/new.py
@@ -1,5 +1,5 @@
 import requests
 response = requests.get(url='https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-txt_file = open("wr.txt", "w")
+txt_file = open("wr.json", "w")
 txt_file.write(response.json())
 txt_file.close()
