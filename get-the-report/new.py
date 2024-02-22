92c6fe009187f824c90acb1d0ff72eb312c5cc43
Liu Hengyuan <110045428+thedeepdeepsky@users.noreply.github.com>
2024-02-22 18:51:56 +0800

    Delete get-the-report/new.py

--- get-the-report/new.py
+++
@@ -1,5 +0,0 @@
-import requests,json
-response = requests.get(url='https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-jsonstr=json.dumps(response,intend=4)
-with open("wr.json") as k:
-  k.write(jsonstr)
